643b34204a41caabb9de9af4dd3f9e793235c9a9
Brandon Miller <brandonjamiller@hotmail.com>
2019-10-28 21:55:28 -0700

    added empty button and header

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import "./styles.scss";
+import React from "react";
+import Header from "./Header"
+
+export default function Appointment(props){
+  return <article className="appointment"></article>
+};
+
