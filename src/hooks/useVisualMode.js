dea0f89488c41207832f3d1f20dc39e6b5696a86
Brandon Miller <brandonjamiller@hotmail.com>
2019-10-30 19:57:31 -0700

    add custom hook, testing for hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+  function transition (newMode, replace) {
+    setMode(newMode)
+    if (replace === true) {
+      // history.pop()
+      // setHistory(history)
+      setHistory([initial]);
+    } else {
+      setHistory([...history, mode])
+    }
+  }
+  function back () { 
+    setMode(history.pop())
+  }
+  return { mode, transition, back }
+  
+} 
